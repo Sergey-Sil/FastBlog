3de30833a93d1d7d1c7ef0be411fba6bea2bae5d
Dmitry Shaido <d.shaido@gmail.com>
2017-12-04 19:57:49 +0300

    added profile editing + changed post view

--- FastBlog/config/routes.rb
+++ FastBlog/config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
 
   namespace :authors do
+    get '/account' => 'accounts#edit', as: :account
+    put '/info' => 'accounts#update_info', as: :info
+    put '/change_password' => 'accounts#change_password', as: :change_password
     resources :posts do
      put 'publish' => 'posts#publish', on: :member
      put 'unpublish' => 'posts#unpublish', on: :member
@@ -13,7 +16,7 @@ Rails.application.routes.draw do
 
   scope module: 'blog' do
     get 'about' => 'pages#about', as: :about
-    get 'contact' => 'pages#about', as: :contact
+    get 'contact' => 'pages#contact', as: :contact
     get 'posts' => 'posts#index', as: :posts
     get 'posts/:id' => 'posts#show', as: :post
 
